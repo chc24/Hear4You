cb7bfd385c084431d07dc39a52683de22940420b
Adam Tache <at201@duke.edu>
2015-11-30 23:51:04 -0500

    Updated login and logout

--- php/login.php
+++ php/login.php
@@ -2,7 +2,7 @@
 require("config.php");
 session_start();
 
-if($_SESSION['logged_in'] !== true) {
+if($_SESSION['logged_in'] == false) {
     if(!empty($_POST)) {
         $query = "
                 SELECT
@@ -20,41 +20,38 @@ if($_SESSION['logged_in'] !== true) {
         $stmt   = $db->prepare($query);
         $result = $stmt->execute($query_params);
         
-    $login_valid = false;
-    $num_row     = 0;
-    
+		$login_valid = false;
+		$num_row     = 0;
+		
         if($result) {
-      $num_row = $stmt->rowCount();
-      if($num_row == 1) {
-        $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $check_password = hash('sha256', $_POST['password'] . $row[0]['salt']);
-        for($round = 0; $round < 65536; $round++) {
-          $check_password = hash('sha256', $check_password . $row[0]['salt']);
-        }
-        
-        if($check_password == $row[0]['password']) {
-          $login_valid = true;
-        }
-        
-        if($login_valid == true) {
-          unset($row['salt']);
-          unset($row['password']);
-          //$_SESSION['user'] = $row['username'];
-          $_SESSION['logged_in'] = true;
-          $_SESSION['username'] = $row[0]['username'];
-          //$_SESSION['uid'] = $row['uid'];
-          echo 'it worked!';
-          header("Location: ../landing.html");
-        } else {
-          echo 'Login Failure';
-        }
-      }
-    }
+			$num_row = $stmt->rowCount();
+			if($num_row >= 1) {
+				$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+				$check_password = hash('sha256', $_POST['password'] . $row[0]['salt']);
+				for($round = 0; $round < 65536; $round++) {
+					$check_password = hash('sha256', $check_password . $row[0]['salt']);
+				}
+				
+				if($check_password == $row[0]['password']) {
+					unset($row['salt']);
+					unset($row['password']);
+					//$_SESSION['user'] = $row['username'];
+					$_SESSION['logged_in'] = true;
+					$_SESSION['username'] = $row[0]['username'];
+					//$_SESSION['uid'] = $row['uid'];
+					header("Location: ../landing.html");
+				} else {
+					echo "Login failure: could not find that username/pass";
+				}
+			} else {
+				echo "Login failure: could not find that username/pass";
+			}
+		}
     } else {
         echo "No POST vars defined";
     }
 } else {
-    header("Location: ../landing.html");
+    echo "You're already logged in. <a href='logout.php'>Logout</a>";
 }
 
 ?>
