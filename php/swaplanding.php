6bee47a187faa6aaf8c8be72f5868a97b486e6fe
Adam Tache <at201@duke.edu>
2015-12-02 20:15:01 -0500

    Implemented resources filtering

--- php/swaplanding.php
+++ php/swaplanding.php
@@ -3,20 +3,15 @@
 
 	if($_SESSION['logged_in'] == true) {
 		echo "<ul class='nav navbar-nav navbar-right'>
-                    <li><a href='index.html#mission'>Mission</a></li>
-                    <li><a href='index.html#roles'>Roles</a></li>
-                    <li><a href='contact.html'>Contact</a></li>
                     <li><a href='resources.html'>Resources</a></li>
+                    <li><a href='contact.html'>Contact</a></li>
                     <li><a href='profile.html'>".$_SESSION['username']."</a></li>
-                    <li><a href='landing.html'><font color='#000000'>Chat Now</font></a></li>
                     <li><a href='php/logout.php'>Sign Out</a></li>
                 </ul>";
 	} else {
 		echo "<ul class='nav navbar-nav navbar-right'>
-                    <li><a href='index.html#mission'>Mission</a></li>
-                    <li><a href='index.html#roles'>Roles</a></li>
-                    <li><a href='contact.html'>Contact</a></li>
                     <li><a href='resources.html'>Resources</a></li>
+                    <li><a href='contact.html'>Contact</a></li>
                     <li><a href='login.html'>Login</a></li>
                 </ul>";
 	}
