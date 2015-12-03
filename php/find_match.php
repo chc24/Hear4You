a80de5342583e4ef1e9cf838114d5b74a3987ef3
Dennis Park <dennisjoonpark@gmail.com>
2015-12-03 05:18:36 -0500

    merge conflict

--- php/find_match.php
+++ php/find_match.php
@@ -2,4 +2,9 @@
 require("config.php");
 session_start();
 
+$query = "SELECT username FROM online WHERE $_SESSION['role'] <> role";
+$stmt   = $db->prepare($query);
+$result = $stmt->execute($query_params);
+
+
 ?>
