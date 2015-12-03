4e187d024e28aea817a83d40913838a83b3ca0de
Dennis Park <dennisjoonpark@gmail.com>
2015-12-03 05:07:55 -0500

    added login and registration updates, authentication tokens for firebase, and matching queries

--- php/check.php
+++ php/check.php
@@ -1,10 +1,37 @@
 <?php
-    require("config.php");
+    //require("config.php");
+    session_start();
 	
-	if($_SESSION['logged_in'] == true){
-		
+	if (isset($_SESSION)) {
+		if($_SESSION['logged_in'] == true){
+			if ($_SESSION['chat_type'] != "group") {
+				header("Location:../individualchat.html");
+			} else {
+				header("Location: ../groupchat.html");
+			}
+			if ($_SESSION['chat_ready'] = true) {
+				header("Location: ../login.html");
+			}
+			//register unsuccessful
+			if($_SESSION['registered'] == false) {
+				header("Location: ../profile.html");
+			} else {
+				// if user is already logged, redirect to landing page
+				header("Location: ../landing.html");
+			}
+		} else {
+			if($_SESSION['registered' == false]) {
+				header("Location: ../registerfail.html");
+			} else {
+				// if user is not logged in, redirect to login page
+				header("Location: ../login.html");
+			}
+		}
 	} else {
-		header("Location: ../index.html");
+		if($_SESSION['registered'] == true) {
+			header("Location: ../landing.html");
+		} else {
+			header("Location: ../index.html");
+		}
 	}
-    
 ?>
--- php/logout.php
+++ php/logout.php
@@ -2,27 +2,25 @@
 require("config.php");
 session_start();
 
-if($_SESSION['logged_in'] == true){
-	//remove the online status of user from db
-	$query = "
-		DELETE FROM online
-		WHERE username = :username";
-	$query_params = array(
-		':username' => $_SESSION['username']
-	);
+//remove the online status of user from db
+$query = "
+	DELETE FROM online
+	WHERE username = :username";
+$query_params = array(
+	':username' => $_SESSION['username']
+);
 
-	//removes all rows matching username
-	// $num_row = 1;
+//removes all rows matching username
+// $num_row = 1;
 
-	// while ($num_row > 0) {
-	// 	$stmt = $db->prepare($query);
-	//  $result = $stmt->execute($query_params);
-	//  $num_row = $stmt->rowCount();
-	// }
+// while ($num_row > 0) {
+// 	$stmt = $db->prepare($query);
+//  $result = $stmt->execute($query_params);
+//  $num_row = $stmt->rowCount();
+// }
 
-	$stmt = $db->prepare($query);
-	$result = $stmt->execute($query_params);
-}
+$stmt = $db->prepare($query);
+$result = $stmt->execute($query_params);
 
 //Clears session cookie and overall session data
 if(isset($_COOKIE[session_name])) {
